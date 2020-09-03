2f356acf83d8e718a407696aa92c5032f3e1af47
Rising Odegua <risingodegua@gmail.com>
2020-09-03 11:16:23 +0100

    Add image

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -28,7 +28,7 @@ export default function Greeting() {
           </div>
         </div>
         <div className="greeting-image-div">
-          <img alt="saad sitting on table" src={require("../../assets/images/manOnTable.svg")}></img>
+          <img alt="Rising" src={require("../../assets/images/rising.png")}></img>
         </div>
       </div>
     </div>
