3ef8ab211f2bd01965a0a15d64ad11502fd4d99e
Rising Odegua <risingodegua@gmail.com>
2020-12-16 05:48:36 +0100

    Updates

---
+++ precache-manifest.cbf1a4e4092ebc89de5224a57b670272.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359b08a30946f3dd7adeca59ca1b511d",
+    "url": "/risingodegua/index.html"
+  },
+  {
+    "revision": "e4064f8730acb46644d6",
+    "url": "/risingodegua/static/css/3.798b2321.chunk.css"
+  },
+  {
+    "revision": "dcc6b9d1fd08a09c5128",
+    "url": "/risingodegua/static/css/4.982340dd.chunk.css"
+  },
+  {
+    "revision": "56d64ed454a341cebe52",
+    "url": "/risingodegua/static/css/main.4ba7d981.chunk.css"
+  },
+  {
+    "revision": "5bab98c5cea8f93ec4b2",
+    "url": "/risingodegua/static/js/2.1df62d8c.chunk.js"
+  },
+  {
+    "revision": "7e0fd0c09333f2e525fa479f52af448a",
+    "url": "/risingodegua/static/js/2.1df62d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4064f8730acb46644d6",
+    "url": "/risingodegua/static/js/3.4d2761e8.chunk.js"
+  },
+  {
+    "revision": "dcc6b9d1fd08a09c5128",
+    "url": "/risingodegua/static/js/4.b97eb72f.chunk.js"
+  },
+  {
+    "revision": "56d64ed454a341cebe52",
+    "url": "/risingodegua/static/js/main.eaacecff.chunk.js"
+  },
+  {
+    "revision": "a17c2efd6e01188ea3c5",
+    "url": "/risingodegua/static/js/runtime-main.be73a384.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/risingodegua/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/risingodegua/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e731cf83eede1b3db2b89186a70ea9e1",
+    "url": "/risingodegua/static/media/access.e731cf83.jpeg"
+  },
+  {
+    "revision": "adb97662e5c2842bd42bcc063e3d1446",
+    "url": "/risingodegua/static/media/alc.adb97662.png"
+  },
+  {
+    "revision": "2bc7787b4376acfbe8262c6f8f8626d6",
+    "url": "/risingodegua/static/media/algo.2bc7787b.jpeg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/risingodegua/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "52a0a17e55606b3c5ffc2835486aed10",
+    "url": "/risingodegua/static/media/crevatal.52a0a17e.png"
+  },
+  {
+    "revision": "687cce1c91377b27a36bc6a1570e546d",
+    "url": "/risingodegua/static/media/datopian-bash-png.687cce1c.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/risingodegua/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "01483323f709c33c870b663055b38081",
+    "url": "/risingodegua/static/media/dsn.01483323.jpg"
+  },
+  {
+    "revision": "669e624b7005ae2fbb1888e7478724d4",
+    "url": "/risingodegua/static/media/experts.669e624b.jpeg"
+  },
+  {
+    "revision": "7f1b4bad1f0a7e652e91054b3d7c1ff3",
+    "url": "/risingodegua/static/media/gcp.7f1b4bad.png"
+  },
+  {
+    "revision": "b4ee78c83df6052f31419bfd1a29dfc2",
+    "url": "/risingodegua/static/media/irsl.b4ee78c8.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/risingodegua/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b5cef5396c028e17c9b23f2b2d35d9a7",
+    "url": "/risingodegua/static/media/microsoft.b5cef539.webp"
+  },
+  {
+    "revision": "fc4f7865bc44b19e3f997f7a0ad22a4e",
+    "url": "/risingodegua/static/media/poster.fc4f7865.jpeg"
+  },
+  {
+    "revision": "78ea30e213edf6a2358dc2318d93aa03",
+    "url": "/risingodegua/static/media/rising.78ea30e2.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/risingodegua/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/risingodegua/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
